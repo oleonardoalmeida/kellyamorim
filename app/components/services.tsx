6f52646833228e5a61263531f2f0dffdfd72e1e1
Leonardo Almeida <cin_lamorim@uolinc.com>
2025-07-03 21:02:31 -0300

    Updated services offer

--- app/components/services.tsx
+++ app/components/services.tsx
@@ -59,7 +59,7 @@ export default function Services() {
           </p>
         </motion.div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8">
           {services.map((service, index) => (
             <motion.div
               key={index}
